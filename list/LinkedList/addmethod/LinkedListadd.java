9e804df332172f108aee94900dacb2bf5dfaa7f4
Suckscorpio999 <olinabin214@gmail.com>
2026-04-12 08:32:01 +0545

    feat : implemented add method in LinkedList for proper fundmental understanding

---
+++ list/LinkedList/addmethod/LinkedListadd.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class LinkedListadd {
+    public static void main(String[] args) {
+
+        LinkedList<String> employees = new LinkedList<String>();
+        employees.add("Nabin");
+        employees.add("Sandip");
+        employees.add("Amrit");
+        employees.add("Diwash");
+        employees.add("Prakash");
+        employees.add("Aditya");
+
+        System.out.println(employees);
+    }
+
+}
