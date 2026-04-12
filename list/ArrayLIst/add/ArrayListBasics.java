ab502bbeec924c3b59d629d98a75b7041e36b47a
Suckscorpio999 <olinabin214@gmail.com>
2026-04-12 08:38:31 +0545

    feat: update project structure

---
+++ list/ArrayLIst/add/ArrayListBasics.java
@@ -0,0 +1,18 @@
+package add;
+import java.util.ArrayList;
+
+public class ArrayListBasics {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.add(1);
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(9);
+        numbers.add(11);
+
+        System.out.println("Numbers in ArrayList are: \n" + numbers);
+
+    }
+}
---
+++ list/ArrayLIst/addallmethod/ArrayListaddall.java
@@ -0,0 +1,18 @@
+package addallmethod;
+import java.util.*;
+
+public class ArrayListaddall {
+    public static void main(String[] args) {
+        ArrayList<String> l1 = new ArrayList<>();
+        l1.add("Java");
+        l1.add("C++");
+
+        ArrayList<String> l2 = new ArrayList<>();
+        l2.add("Python");
+        l2.add("C");
+
+        l1.addAll(l2);
+
+        System.out.println("" + l1);
+    }
+}
