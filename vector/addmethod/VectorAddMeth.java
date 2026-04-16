a43554303619564991d3352f925d490f8aa249ca
Suckscorpio999 <olinabin214@gmail.com>
2026-04-16 21:22:16 +0545

    feat: implemented add method in vector collection in java

---
+++ vector/addmethod/VectorAddMeth.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class VectorAddMeth {
+    public static void main(String[] args) {
+        Vector<String> employees = new Vector<String>();
+        employees.add("Sandeep");
+        employees.add("Amirtes");
+        employees.add("Diwash");
+        employees.add("Prakash");
+        employees.add("Sachin");
+        System.out.println(employees);
+    }
+}
